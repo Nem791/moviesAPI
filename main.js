9b8c01106aae4c1fa6f2b80260a0562ff0493c74
Nem791 <phuongnam131201.nnnpn@gmail.com>
2021-07-21 15:24:08 +0700

    mindX API movie

--- main.js
+++ main.js
@@ -23,7 +23,10 @@ server.use((req, res, next) => {
 })
 
 // Use default router
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+server.use('/api', router);
+
+// Start sever
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
